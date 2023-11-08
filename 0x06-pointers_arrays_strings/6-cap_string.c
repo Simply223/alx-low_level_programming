75f3db6884a9f277ace53aa1866ae5c3af2e1929
Simply223 <kiplagatshawn@ieee.org>
2023-11-08 10:46:24 -0800

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,25 +8,24 @@
 
 char *cap_string(char *s)
 {
-  int i = 0, j;
-  int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-  if (*(s + i) >= 97&& *(s + i) <= 122)
-    *(s + i) = *(s + i) -32;
-  i++;
-
-  while (*(s + i) != '\0')
-    {
-      for (j = 0; j < 13; j++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-	  if (*(s + i) = sep_words[j])
-	    {
-	      if ((*(s + (i + 1)) >= 97) && (*(s + (i + 1))  <= 122))
-		*(s + (i + 1)) = *(s + (i + 1)) - 32;
-	      break;
-	    }
+		for (i = 0; i < 13; i++)
+		{
+			if (*(s + count) == sep_words[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
+		}
+		count++;
 	}
-      i++;
-    }
-  return (s);
+	return (s);
 }
