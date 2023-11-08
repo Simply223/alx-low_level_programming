1ea43fb058ed238eead0180e975e2f050c4ccb33
Simply223 <kiplagatshawn@ieee.org>
2023-11-08 08:53:18 -0800

    strncpy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-#inlcude "main.h"
+#include "main.h"
 /**
  * _strncat - concatenates two strings,
  * @dest: destination.
@@ -8,17 +8,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-  int i = 0, j = 0;
+	int i = 0, j = 0;
 
-  while (*(dest + i) != '\0')
-    i++;
-  while (j < n)
-    {
-      *(dest = i) = *(src + j);
-      if (*(src + j) == '\0')
-	break;
-      i++;
-      j++;
-    }
-  return (dest);
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while (j < n)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+			break;
+		i++;
+		j++;
+	}
+	return (dest);
 }
